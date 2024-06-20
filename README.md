08825050f1bc45a698113429151056e6844e5be2
peanuts359 <aaron.sim.359@gmail.com>
2024-06-20 14:56:01 +0800

    Wrote first components

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# odin-cv
-The Odin Project - React Project
+The Odin Project - React Project 1
+
+A simple webpage to upload a CV/Resume to. Made with React among other things.
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <a href="https://www.theodinproject.com/dashboard" target="_blank">
+                {" "}
+                The Odin Project
+            </a>
+        </div>
+    );
+}
+
+export default Footer;
